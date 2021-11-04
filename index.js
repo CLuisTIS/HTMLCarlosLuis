0b215cf170ee5f7ec878a195b0962a290e5cbb75
CLuisTIS <carlosluistis@outlook.pt>
2021-11-04 12:35:14 -0100

    html e seu codigo

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const { dirname } = require('path/posix')
 const app = express()
+const path = require('path')
  
 app.use(express.static('./public'))
 
@@ -8,18 +10,20 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/:nome',(req, res) => {
-    res.json('Hello ' + req.params.nome)
-  })
-
-  //FUNCTION FOI SUBSTITUIDO POR =>
+ //FUNCTION FOI SUBSTITUIDO POR =>
 //app.get('/',(req, res) => {
 //    res.send('Hello World')
 //})
 
+app.get('/:nome',(req, res) => {
+    res.json('Hello ' + req.params.nome)
+  })
+
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'))
+})
+
 const port = 3000
-
-
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
